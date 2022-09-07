f676f52000557a442aca408a89969d4225746bf8
Matt-Gibson PVWD <gibson.matthew@hotmail.com>
2022-09-07 13:34:41 -0400

    Added Parseing and Concatenation Examples

--- Program.cs
+++ Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Try Something New");
+
+            //Retuning an entered whole number with Parseing
+            Console.Write("Enter a Whole Number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine($"You entered: {number}.");
+           
+            //Returning an entered decimal with Parseing
+            Console.Write("Enter a decimal number: ");
+            decimal a = decimal.Parse(Console.ReadLine());
+            Console.WriteLine($"You entered {a}.");
+
+            //Concatenating Strings
+            Console.Write("Enter your first name: ");
+            string firstName = Console.ReadLine();
+            Console.Write("Enter your last name: ");
+            string lastName = Console.ReadLine();
+            Console.WriteLine($"You name is {firstName} {lastName}.");
+
+
         }
     }
 }

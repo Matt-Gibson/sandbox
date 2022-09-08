0d122c82c878bf4c1b7e08d79359f4a5b91ab222
Matthew Gibson <gibson.matthew@hotmail.com>
2022-09-08 07:54:08 -0400

    Add new file for Structure class

---
+++ Structure.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Structure
+{
+	private int width;
+	private int height;
+	private int length;
+
+	public static void SetWidth() { }
+
+	public static void SetHeight() { }
+
+	public static void SetLength() { }
+
+}

54049932a9350fe3641160477c3e1ddf9b9bec0d
Matthew Gibson <gibson.matthew@hotmail.com>
2022-09-09 22:21:26 -0400

    Change variable declarations to static with _naming standard for private and filled out Set methods

--- Structure.cs
+++ Structure.cs
@@ -2,14 +2,23 @@
 
 public class Structure
 {
-	private int width;
-	private int height;
-	private int length;
+	private static int _width;
+	private static int _height;
+	private static int _length;
 
-	public static void SetWidth() { }
+	public static void SetWidth(int Width)
+	{
+		_width = Width;
+	}
 
-	public static void SetHeight() { }
+	public static void SetHeight(int Height)
+	{
+		_height = Height;
+	}
 
-	public static void SetLength() { }
+	public static void SetLength(int Length)
+	{
+		_length = Length;
+	}
 
 }

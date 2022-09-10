763d4653ecc777facdcb88226b66a350514d7470
Matthew Gibson <gibson.matthew@hotmail.com>
2022-09-10 11:32:00 -0400

    Change string to string? in NullTest

--- NullCheck.cs
+++ NullCheck.cs
@@ -2,7 +2,8 @@ using System;
 
 public class NullCheck
 {
-    public static string String(string test)
+    //Not placing the ? after string was making the test case always be true, as per rider help message
+    public static string String(string? test)
     {
         if (test == null)
         {

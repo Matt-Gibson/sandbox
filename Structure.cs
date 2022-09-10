4817139f64ef3d7b0eb76c722dd28450e5570861
Matthew Gibson <gibson.matthew@hotmail.com>
2022-09-09 22:26:05 -0400

    Add comments to Structure.cs

--- Structure.cs
+++ Structure.cs
@@ -2,10 +2,12 @@
 
 public class Structure
 {
+	//Set to private to ensure control of input
 	private static int _width;
 	private static int _height;
 	private static int _length;
 
+	//Methods to set properties
 	public static void SetWidth(int Width)
 	{
 		_width = Width;
